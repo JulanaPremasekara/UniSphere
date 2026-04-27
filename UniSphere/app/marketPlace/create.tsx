d42c32ceb0a12a2942ba9be2440f5e998ff955d2
JAVA <jayanandith123@gmail.com>
2026-04-27 13:18:04 +0530

    resolve conflict

--- UniSphere/app/marketPlace/create.tsx
+++ UniSphere/app/marketPlace/create.tsx
@@ -20,10 +20,6 @@ import { VStack } from "@/components/ui/vstack";
 import { HStack } from "@/components/ui/hstack";
 import { Text } from "@/components/ui/text";
 import { Button, ButtonText } from "@/components/ui/button";
-<<<<<<< HEAD
-
-=======
->>>>>>> 25a4edbe06043e3b2042c9fb845a6ae0a4a50681
 import apiClient from "../services/api";
 
 const CONDITIONS = ["New", "Used", "Like New"];
