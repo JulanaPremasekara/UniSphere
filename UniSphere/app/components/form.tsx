c2ad4660f69c15c9758f7a0aedee23180364e5d9
IT24102465 <it24102465@my.sliit.lk>
2026-05-03 20:18:59 +0530

    frontend updated

--- UniSphere/app/components/form.tsx
+++ UniSphere/app/components/form.tsx
@@ -13,47 +13,73 @@ interface LoginFormProps {
   errorMessage: string | null;
 }
 
-export default function LoginForm({ email, setEmail, password, setPassword, errorMessage }: LoginFormProps) {
-  const Field = ({ label, place, val, setVal, type = 'text', isPass }: any) => {
-    const [showPassword, setShowPassword] = useState(false);
+interface FieldProps {
+  label: string;
+  place: string;
+  val: string;
+  setVal: (val: string) => void;
+  type?: "text" | "password";
+  isPass?: boolean;
+  isInvalid?: boolean;
+  errorMessage?: string | null;
+}
 
-    return (
-      <FormControl isInvalid={!!errorMessage} size="md">
-        <FormControlLabel>
-          <FormControlLabelText className="text-gray-700 font-semibold mb-1">{label}</FormControlLabelText>
-        </FormControlLabel>
-        <Input className="h-14 rounded-[20px] bg-gray-50 border-transparent px-2">
-          <InputField 
-            type={isPass ? (showPassword ? 'text' : 'password') : type} 
-            placeholder={place} 
-            value={val} 
-            onChangeText={setVal} 
-            className="text-gray-800" 
-          />
-          {isPass && (
-            <InputSlot className="pr-3" onPress={() => setShowPassword(!showPassword)}>
-              <InputIcon as={showPassword ? Eye : EyeOff} size="lg" className="text-gray-400" />
-            </InputSlot>
-          )}
-        </Input>
-        {isPass && errorMessage ? (
-          <FormControlError className="mt-2">
-            <FormControlErrorIcon as={AlertCircleIcon} />
-            <FormControlErrorText>{errorMessage}</FormControlErrorText>
-          </FormControlError>
-        ) : isPass ? (
-          <FormControlHelper>
-            <FormControlHelperText className="text-right mt-2 text-indigo-600 font-medium">Forgot Password?</FormControlHelperText>
-          </FormControlHelper>
-        ) : null}
-      </FormControl>
-    );
-  };
+const Field = ({ label, place, val, setVal, type = 'text', isPass, isInvalid, errorMessage }: FieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
+    <FormControl isInvalid={isInvalid} size="md">
+      <FormControlLabel>
+        <FormControlLabelText className="text-gray-700 font-semibold mb-1">{label}</FormControlLabelText>
+      </FormControlLabel>
+      <Input className="h-14 rounded-[20px] bg-gray-50 border-transparent px-2">
+        <InputField 
+          type={isPass ? (showPassword ? 'text' : 'password') : type} 
+          placeholder={place} 
+          value={val} 
+          onChangeText={setVal} 
+          className="text-gray-800" 
+        />
+        {isPass && (
+          <InputSlot className="pr-3" onPress={() => setShowPassword(!showPassword)}>
+            <InputIcon as={showPassword ? Eye : EyeOff} size="lg" className="text-gray-400" />
+          </InputSlot>
+        )}
+      </Input>
+      {isPass && errorMessage ? (
+        <FormControlError className="mt-2">
+          <FormControlErrorIcon as={AlertCircleIcon} />
+          <FormControlErrorText>{errorMessage}</FormControlErrorText>
+        </FormControlError>
+      ) : isPass ? (
+        <FormControlHelper>
+          <FormControlHelperText className="text-right mt-2 text-indigo-600 font-medium">Forgot Password?</FormControlHelperText>
+        </FormControlHelper>
+      ) : null}
+    </FormControl>
+  );
+};
+
+export default function LoginForm({ email, setEmail, password, setPassword, errorMessage }: LoginFormProps) {
+  return (
     <VStack space="xl">
-      <Field label="Email Address" place="Enter your email" val={email} setVal={setEmail} />
-      <Field label="Password" place="Enter your password" val={password} setVal={setPassword} type="password" isPass={true} />
+      <Field 
+        label="Email Address" 
+        place="Enter your email" 
+        val={email} 
+        setVal={setEmail} 
+        isInvalid={!!errorMessage} 
+      />
+      <Field 
+        label="Password" 
+        place="Enter your password" 
+        val={password} 
+        setVal={setPassword} 
+        type="password" 
+        isPass={true} 
+        isInvalid={!!errorMessage} 
+        errorMessage={errorMessage}
+      />
     </VStack>
   );
 }
